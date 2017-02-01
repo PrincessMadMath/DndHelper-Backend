e7e11647fc3ce0c27241fbd496f9e92f57ddd8f8
Mathieu <math.gamache@gmail.com>
2017-02-01 09:46:41 -0500

    Remove travis support
    * Not liking the way to encryp key for file pipeline
    * Somme issue with encryption when on windows: https://github.com/travis-ci/travis-ci/issues/4746, http://stackoverflow.com/questions/40814626/travis-ci-bad-decrypt-in-windows-10
    
    -> Trying budy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.json({"message": "Hello World!"})
+  res.json({"message": "Hello World 2!!!"})
 })
 
 const PORT = process.env.PORT || 8080;
