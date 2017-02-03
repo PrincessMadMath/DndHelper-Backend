fdb8baa5d3cde466994a4ef72779f9710d9a0cc2
Mathieu <math.gamache@gmail.com>
2017-02-02 22:25:39 -0500

    Same thing...

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.json({"message": "Hello World"})
+  res.json({"message": "Hello World!"})
 })
 
 const PORT = process.env.PORT || 8080;
