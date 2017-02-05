cde5ccc4bd3264e9d4ac40377807895592d4443f
Mathieu Gamache <mathg99@hotmail.com>
2017-02-05 01:42:05 -0500

    Still trying to test...

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.json({"message": "Hello World!"})
+  res.json({"message": "Hello World!", "secret" : process.env.TEST_VALUE})
 })
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT} and value ${process.env.TEST_VALUE}!`)
+  console.log(`Example app listening on port ${PORT}!`)
 })
