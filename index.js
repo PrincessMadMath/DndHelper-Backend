9593785aa7e035fb07ca652a58bdab5357ba5099
Mathieu <math.gamache@gmail.com>
2017-02-18 20:24:17 -0500

    Allowing cross origin for pam.netlify

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 var express = require('express')
 var bodyParser = require('body-parser')
+var cors = require("cors");  
 
 var app = express()
 
+app.use(cors({  
+    origin: ["https://pam.netlify.com/"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 // parse application/json
 app.use(bodyParser())
 
